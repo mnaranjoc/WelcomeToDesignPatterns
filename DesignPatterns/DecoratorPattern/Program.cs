8fdb8a2c498399c9b097ad7ff694045fa6247f26
Manuel Naranjo <manuelnaranjo@outlook.com>
2019-07-31 17:36:53 -0500

    Decorator pattern practice

---
+++ DesignPatterns/DecoratorPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beverage beverage = new Espresso();
+            // Make it double mocha
+            beverage = new Mocha(beverage);
+            beverage = new Mocha(beverage);
+            Console.WriteLine(string.Format("Beverage: {0}\nCost: {1}\n", beverage.getDescription(), beverage.cost()));
+
+            Console.ReadLine();
+        }
+    }
+}
