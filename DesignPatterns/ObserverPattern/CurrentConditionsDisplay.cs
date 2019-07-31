0f2cd3f798c152611e6adbb24fe942082b756fff
Manuel Naranjo <45862940+mnaranjoc@users.noreply.github.com>
2019-07-31 13:37:45 -0500

    Chapter 2 excersices finished (#2)

---
+++ DesignPatterns/ObserverPattern/CurrentConditionsDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern
+{
+    class CurrentConditionsDisplay : Observer, DisplayElement
+    {
+        private float temperature;
+        private float humidity;
+        private Subject weatherData;
+
+        public CurrentConditionsDisplay(Subject _weatherData)
+        {
+            this.weatherData = _weatherData;
+            _weatherData.registerObserver(this);
+        }
+
+        public void display()
+        {
+            Console.WriteLine(string.Format("Current conditions: {0}F degrees and {1} humidity.", temperature, humidity));
+        }
+
+        public void update(float temp, float humidity, float pressure)
+        {
+            this.temperature = temp;
+            this.humidity = humidity;
+            display();
+        }
+    }
+}
---
+++ DesignPatterns/ObserverPattern/WeatherData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern
+{
+    class WeatherData : Subject
+    {
+        private List<Observer> observers;
+        private float temperature;
+        private float humidity;
+        private float pressure;
+
+        public WeatherData()
+        {
+            observers = new List<Observer>();
+        }
+
+        public void notifyObservers()
+        {
+            foreach(var observerObject in observers)
+            {
+                observerObject.update(temperature, humidity, pressure);
+            }
+        }
+
+        public void registerObserver(Observer o)
+        {
+            observers.Add(o);
+        }
+
+        public void removeObserver(Observer o)
+        {
+            observers.Remove(o);
+        }
+
+        public void measurementsChanged()
+        {
+            this.notifyObservers();
+        }
+
+        public void setMeasurements(float temp, float humidity, float pressure)
+        {
+            this.temperature = temp;
+            this.humidity = humidity;
+            this.pressure = pressure;
+            this.measurementsChanged();
+    }
+    }
+}
