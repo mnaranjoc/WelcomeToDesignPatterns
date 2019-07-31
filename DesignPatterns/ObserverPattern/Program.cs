20b89ac81a0789a83457051237483607e8043ffa
Manuel Naranjo <manuelnaranjo@outlook.com>
2019-07-31 13:36:27 -0500

    Chapter 2 excersices finished

---
+++ DesignPatterns/ObserverPattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern
+{
+    class Program
+    {
+        // This example is not entirely good because the subject pushes the data to observers
+        // and is not flexible. If there was a new attribute we will have to modify all the 
+        // observers update methods. It will be better if we could pull the info from the observers.
+        static void Main(string[] args)
+        {
+            WeatherData weatherData = new WeatherData();
+
+            CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+
+            weatherData.setMeasurements(80, 65, 30.4f);
+            weatherData.setMeasurements(82, 70, 29.2f);
+            weatherData.setMeasurements(78, 90, 29.2f);
+
+            Console.ReadLine();
+        }
+    }
+}
