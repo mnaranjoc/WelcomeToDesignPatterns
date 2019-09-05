c651d6dde5af0c99c34494c2ddf2d301c66cd54a
Manuel Naranjo Contreras <manuelnaranjo@outlook.com>
2019-09-05 17:13:10 -0500

    Singleton exercises finished

--- DesignPatterns/SingletonPattern/Program.cs
+++ DesignPatterns/SingletonPattern/Program.cs
@@ -10,6 +10,13 @@ namespace SingletonPattern
     {
         static void Main(string[] args)
         {
+            var singleton = Singleton.getInstance("manuel");
+            singleton.hello();
+
+            var singleton2 = Singleton.getInstance("nelly");
+            singleton2.hello();
+
+            Console.ReadLine();
         }
     }
 }
---
+++ DesignPatterns/SingletonPattern/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    // Simple singleton
+    class Singleton
+    {
+        private static Singleton singleton;
+        private string name;
+
+        private Singleton() { }
+
+        public static Singleton getInstance(string _name)
+        {
+            if (singleton == null)
+            {
+                singleton = new Singleton();
+                singleton.name = _name;
+            }
+
+            return singleton;
+        }
+
+        public void hello()
+        {
+            Console.WriteLine(string.Format("Hello: {0}", name));
+        }
+    }
+}
