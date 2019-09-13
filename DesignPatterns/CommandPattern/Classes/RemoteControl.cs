669c791d00c6d069cf722189932523c89a6fa6b4
Manuel Naranjo Contreras <manuelnaranjo@outlook.com>
2019-09-12 23:28:26 -0500

    Excercise 2 beginning

---
+++ DesignPatterns/CommandPattern/Classes/RemoteControl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern.Classes
+{
+    class RemoteControl
+    {
+        ICommand[] onCommands;
+        ICommand[] offCommands;
+
+        public RemoteControl()
+        {
+            onCommands = new ICommand[7];
+            offCommands = new ICommand[7];
+
+            ICommand noCommand = new NoCommand();
+            for(int i=0; i<7; i++)
+            {
+                onCommands[i] = noCommand;
+                offCommands[i] = noCommand;
+            }
+        }
+
+        public void setCommand(int slot, ICommand onCommand, ICommand offCommand)
+        {
+            onCommands[slot] = onCommand;
+            offCommands[slot] = offCommand;
+        }
+    }
+}
