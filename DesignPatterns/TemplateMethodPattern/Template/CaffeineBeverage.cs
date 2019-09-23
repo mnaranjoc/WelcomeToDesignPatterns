14d5ba66141ecc1d2228da5ae5c59ca2786c9900
Manuel Naranjo Contreras <manuelnaranjo@outlook.com>
2019-09-23 07:51:30 -0500

    Template Method Pattern excercise

---
+++ DesignPatterns/TemplateMethodPattern/Template/CaffeineBeverage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodPattern.Template
+{
+    public abstract class CaffeineBeverage
+    {
+        void prepareReceipe()
+        {
+            boilWater();
+            brew();
+            pourInCoup();
+            addCondiments();
+        }
+
+        public abstract void addCondiments();
+
+        public void pourInCoup()
+        {
+            Console.WriteLine("Pouring into coup");
+        }
+
+        public abstract void brew();
+
+        public void boilWater()
+        {
+            Console.WriteLine("Boiling condiments");
+        }
+    }
+}
---
+++ DesignPatterns/TemplateMethodPattern/Template/Tea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodPattern.Template
+{
+    class Tea : CaffeineBeverage
+    {
+        public override void addCondiments()
+        {
+            Console.WriteLine("Adding lemon");
+        }
+
+        public override void brew()
+        {
+            Console.WriteLine("Steeping the tea");
+        }
+    }
+}
