8494c01fc7802b58602a49eed128cb257f9f5a18
Manuel Naranjo Contreras <manuelnaranjo@outlook.com>
2019-09-23 17:07:35 -0500

    Template Method Pattern compareTo excercise

---
+++ DesignPatterns/TemplateMethodPattern/CompareTo/Duck.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodPattern.CompareTo
+{
+    class Duck : IComparable
+    {
+        int weight;
+        string name;
+
+        public Duck(int weight, string name)
+        {
+            this.weight = weight;
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            string ret = this.name + "weighs " + weight.ToString();
+            return ret;
+        }
+
+        public int CompareTo(Object obj)
+        {
+            Duck objDuck = obj as Duck;
+            if (objDuck != null)
+            {
+                if (this.weight < objDuck.weight)
+                    return -1;
+                else if (this.weight == objDuck.weight)
+                    return 0;
+                else
+                    return 1;
+            }
+            else
+                return 1;
+        }
+    }
+}
--- DesignPatterns/TemplateMethodPattern/Program.cs
+++ DesignPatterns/TemplateMethodPattern/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TemplateMethodPattern.CompareTo;
 using TemplateMethodPattern.Template;
 
 namespace TemplateMethodPattern
@@ -11,6 +12,38 @@ namespace TemplateMethodPattern
     {
         static void Main(string[] args)
         {
+            //Program.templateTesting();
+            Program.compareToTesting();
+
+            Console.ReadLine();
+        }
+
+        private static void compareToTesting()
+        {
+            Duck[] ducks = {
+                new Duck(8, "Daffy"),
+                new Duck(2, "Dewey"),
+                new Duck(7, "Howard"),
+                new Duck(2, "Louie"),
+                new Duck(10, "Donald"),
+                new Duck(2, "Huey")
+            };
+
+            Console.WriteLine("Before sorting\n--------------");
+
+            foreach (var duck in ducks)
+                Console.WriteLine(duck.ToString());
+
+            Array.Sort(ducks);
+
+            Console.WriteLine("\nAfter sorting\n--------------");
+
+            foreach (var duck in ducks)
+                Console.WriteLine(duck.ToString());
+        }
+
+        public static void templateTesting()
+        {
             CaffeineBeverage cofee = new Coffee();
             cofee.prepareReceipe();
 
@@ -18,8 +51,6 @@ namespace TemplateMethodPattern
 
             CaffeineBeverage tea = new Tea();
             tea.prepareReceipe();
-
-            Console.ReadLine();
         }
     }
 }
