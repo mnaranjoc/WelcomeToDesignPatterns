3bd50bc73110e73b1b81b08b9a9008e41fcae0ca
Manuel Naranjo Contreras <manuelnaranjo@outlook.com>
2019-09-24 12:26:56 -0500

    Removing all unnecesary using references

--- DesignPatterns/IteratorAndCompositePattern/MenuItem.cs
+++ DesignPatterns/IteratorAndCompositePattern/MenuItem.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IteratorAndCompositePattern
+﻿namespace IteratorAndCompositePattern
 {
     class MenuItem
     {
