20ed229f526959fa1da3aebcb3e74f71308d3687
Manuel Naranjo Contreras <manuelnaranjo@outlook.com>
2019-09-30 11:51:27 -0500

    State Pattern excercise 1

--- DesignPatterns/StatePattern/Program.cs
+++ DesignPatterns/StatePattern/Program.cs
@@ -10,6 +10,40 @@ namespace StatePattern
     {
         static void Main(string[] args)
         {
+            GumballMachine machine = new GumballMachine(5);
+
+            Console.WriteLine(machine);
+
+            machine.insertQuarter();
+            machine.turnCrank();
+
+            Console.WriteLine(machine);
+
+            machine.insertQuarter();
+            machine.ejectQuarter();
+            machine.turnCrank();
+
+            Console.WriteLine(machine);
+
+            machine.insertQuarter();
+            machine.turnCrank();
+            machine.insertQuarter();
+            machine.turnCrank();
+            machine.ejectQuarter();
+
+            Console.WriteLine(machine);
+
+            machine.insertQuarter();
+            machine.insertQuarter();
+            machine.turnCrank();
+            machine.insertQuarter();
+            machine.turnCrank();
+            machine.insertQuarter();
+            machine.turnCrank();
+
+            Console.WriteLine(machine);
+
+            Console.ReadLine();
         }
     }
 }
